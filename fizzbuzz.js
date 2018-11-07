911855ccbd21ebfa489378dacd4efd4cc126ae86
iykeevans <elochi238@gmail.com>
2018-11-07 21:24:07 +0100

    created logic for fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,19 @@
+module.exports = function(n) {
+    if (typeof n !== 'number') {
+        return null;
+    }
+
+    if (n % 15 === 0) {
+        return 'fizzbuzz';
+    }
+
+    if (n % 3 === 0) {
+        return 'fizz';
+    }
+
+    if (n % 5 === 0) {
+        return 'buzz';
+    }
+
+    return '' + n;
+};
